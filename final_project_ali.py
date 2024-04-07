d4a52f285efaa2c062ed59b08aa23f1d483aca3b
ceylster <acceylan96@gmail.com>
2024-04-08 02:18:59 +0300

    bu sefer orijinali geliyor

--- final_project_ali.py
+++ final_project_ali.py
@@ -1,154 +1,173 @@
-import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns
-from matplotlib import pyplot as plt
-# !pip install missingno
-import missingno as msno
-from datetime import date
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import LocalOutlierFactor
+import numpy as np
+from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler, RobustScaler
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score,roc_auc_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import confusion_matrix
+from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier, AdaBoostClassifier
+from sklearn.model_selection import cross_validate, GridSearchCV, train_test_split
+from sklearn.metrics import roc_curve, auc, classification_report, confusion_matrix, accuracy_score
+from sklearn.multiclass import OneVsRestClassifier
+import warnings
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
-pd.set_option('display.float_format', lambda x: '%.3f' % x)
-pd.set_option('display.width', 500)
+pd.set_option('display.width', 1000)
+
+df = pd.read_csv("Miull/Projects_doc/winequalityN.csv")
+
+def check_df(dataframe, head=5):
+    print(10*"#" + " Shape ".center(9) + 10*"#")
+    print(dataframe.shape)
+    print(10*"#" + " Types ".center(9) + 10*"#")
+    print(dataframe.dtypes)
+    print(10*"#" + " Head ".center(9) + 10*"#")
+    print(dataframe.head(head))
+    print(10*"#" + " Tail ".center(9) + 10*"#")
+    print(dataframe.tail(head))
+    print(10*"#" + " NA ".center(9) + 10*"#")
+    print(dataframe.isnull().sum())
+    print(10*"#" + " Quantiles ".center(9) + 10*"#")
+    print(dataframe.describe([0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]).T)
+    print(10*"#" + " Unique Values ".center(9) + 10*"#")
+    print(dataframe.nunique())
+check_df(df)
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal degişkenlerin isimlerini verir.
+
+    Parameters
+    ----------
+    dataframe:dataframe
+        değişken isimleri alınmak istenen dataframe'dir.
+    cat_th:int,float
+        numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+    car_th: int, float
+        kategorik fakat kardinal degişkenler için sınıf eşik değeri
+
+    Returns
+    -------
+    cat_cols: list
+        Kategorik değişen listesi
+    num_cols: list
+        Numerik degisken listesi
+    cat_but_car: list
+        Kategorik görünümlü kardinal değişken listesi
+
+    Notes
+    ------
+    cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+    num_but_cat cat_cols'un icerisinde.
 
 
-df = pd.read_csv('Final Project/winequalityN.csv')
-
-df.head()
-df.info()
-
-df['quality'].nunique()
-df['fixed acidity'].nunique()
+    """
 
 
-df.groupby('type')["quality"].mean()
-df.groupby('quality')["fixed acidity"].mean()
-# 7 unique quality numbers but actually 10 in total
-df["quality"].describe().T
+    cat_cols = [col for col in dataframe.columns if str(dataframe[col].dtypes) in ["category", "object", "bool"]]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < 10 and dataframe[col].dtypes in ["int64", "float64", "int32", "float32"]]
+    cat_but_car = [col for col in dataframe.columns if
+                   dataframe[col].nunique() > 20 and str(dataframe[col].dtypes) in ["object", "category"]]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
 
-def get_col_names(dataframe, cat_th=10, car_th = 20):
-
-    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == 'O']
-    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
-                   dataframe[col].dtypes != 'O']
-    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
-                   dataframe[col].dtypes == 'O']
-
-    # Categorical Columns
-
-    categorical_columns = cat_cols + num_but_cat
-    categorical_columns = [col for col in categorical_columns if col not in categorical_columns]
-
-    # Numerical Columns
-    numerical_columns = [col for col in dataframe.columns if dataframe[col].dtypes != 'O']
-    numerical_columns = [col for col in numerical_columns if col not in num_but_cat]
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes in ["int64", "float64", "int32", "float32"]]
+    num_cols = [col for col in dataframe.columns if col not in cat_cols]
 
     print(f"Observations: {dataframe.shape[0]}")
     print(f"Variables: {dataframe.shape[1]}")
-    print(f'Number of Categorical Columns: {len(cat_cols)}')
-    print(f'Number of Numerical Columns: {len(numerical_columns)}')
-    print(f'Number of Hidden Cardinal Columns: {len(cat_but_car)}')
-    print(f'Number of Categorical Columns But Stored As Numerical in Dataframe: {len(num_but_cat)}')
-    print(f'Categorical Columns But Stored as Numerical in Dataframe: {num_but_cat}')
-    return cat_cols, numerical_columns, cat_but_car
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_car: {len(num_but_cat)}')
+
+    return cat_cols, num_cols, cat_but_car
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+df["type"] = LabelEncoder().fit_transform(df["type"])
+
+# ----------------------------------------------------------------------------------------------------------------------
+df.dropna(inplace=True)
+
+y = df["quality"]
+X = df.drop(["quality"], axis=1)
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25)
+
+RF=OneVsRestClassifier(RandomForestClassifier(max_features=0.2))
+RF.fit(X_train,y_train)
+y_pred = RF.predict(X_test)
+pred_prob=RF.predict_proba(X_test)
+pred_prob.shape
+
+RF.score(X_test,y_test)
+
+# ----------------------------------------------------------------------------------------------------------------------
 
 
-get_col_names(df)
-# Check for missing values
-df.isnull().sum()
-
-# Filling missing values with their mean values
-for col in df.columns:
-  if df[col].isnull().sum() > 0:
-    df[col] = df[col].fillna(df[col].mean())
-
-# Check for outliers
-
-def outlier_thresholds(dataframe, col_name, q1=0.05, q3=0.95):
+def outlier_thresholds(dataframe, col_name, q1, q3):
     quartile1 = dataframe[col_name].quantile(q1)
     quartile3 = dataframe[col_name].quantile(q3)
     interquantile_range = quartile3 - quartile1
     up_limit = quartile3 + 1.5 * interquantile_range
     low_limit = quartile1 - 1.5 * interquantile_range
     return low_limit, up_limit
-def check_outlier(dataframe, col_name):
-    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
-    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
-        return True
-    else:
-        return False
 
-def grab_outliers(dataframe, col_name, index=False):
-    low, up = outlier_thresholds(dataframe, col_name)
-
-    if dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].shape[0] > 10:
-        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].head())
-    else:
-        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))])
-
-    if index:
-        outlier_index = dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].index
-        return outlier_index
-
-num_cols = df.drop(["type", "quality"], axis = 1)
-for col in num_cols:
-    if check_outlier(df, col) == True:
-        grab_outliers(df, col)
-        print(col, outlier_thresholds(df, col))
-
-def replace_with_thresholds(dataframe, variable):
-    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+def replace_with_thresholds(dataframe, variable, q1, q3):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable, q1, q3)
     dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
     dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
 
 for col in num_cols:
-    if check_outlier(df, col) == True:
-        replace_with_thresholds(df, col)
-        print(col, outlier_thresholds(df, col))
+    replace_with_thresholds(df, col, 0.05, 0.95)
 
-for col in num_cols:
-     print(col, check_outlier(df, col))
+df.isnull().sum()
 
-#df.describe()
+for col in df.columns:
+    df[col].fillna(df[col].median(), inplace=True)
 
-"""""
-sns.set()
-plt.figure(figsize=(20,10))
-sns.boxplot(data=df,palette="Set3")
-plt.show()
-"""""
+df["total acidity"] = df["fixed acidity"] + df["volatile acidity"] + df["citric acid"]
 
-# len(df[df['free sulfur dioxide'] > 83])
-# len(df[df['total sulfur dioxide'] > 283])
-# len(df[df['residual sugar'] > 20])
+df["residul sugar levels"] = pd.cut(df["residual sugar"],
+                                    bins=[0, 0.6, 10, 20, 70, 1000],
+                                    labels=["bone_dry", "dry", "off_dry", "medium_dry", "sweet"])
 
-df["type"].value_counts()
-df.groupby(['type', 'quality']).agg({"pH": "mean"})
+df["alcohol levels"] = pd.cut(df["alcohol"], bins=[0, 12.5, 13.5, 14.5, 20],
+                              labels=["low", "moderately_low", "high", "very_high"])
 
-le = LabelEncoder()
-df['type'] = le.fit_transform(df["type"])
-le.inverse_transform([0, 1])
+df["white perfect pH"] = np.where((df["type"] == 1) & (df["pH"] >= 3) & (df["pH"] <= 3.3), 1, 0)
 
-df.head()
-df['quality'].value_counts()
+df["red perfect pH"] = np.where((df["type"] == 0) & (df["pH"] >= 3.3) & (df["pH"] <= 3.6), 1, 0)
 
-y = df["quality"]
-X = df.drop("quality", axis=1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=17)
+df["Cquality"] = pd.cut(df["quality"], bins=[0, 4, 6, 10], labels=["bad", "good", "perfect"])
 
-dtree_model = DecisionTreeClassifier(max_depth = 2).fit(X_train, y_train)
-dtree_predictions = dtree_model.predict(X_test)
-y_pred = dtree_model.predict(X_test)
-cm = confusion_matrix(y_test, dtree_predictions)
-print(cm)
+df["residul sugar levels"] = LabelEncoder().fit_transform(df["residul sugar levels"])
+df["alcohol levels"] = LabelEncoder().fit_transform(df["alcohol levels"])
+df["Cquality"] = LabelEncoder().fit_transform(df["Cquality"])
 
+df.drop(columns=["quality"], inplace=True, axis=1)
 
-print(f"Accuracy: {round(accuracy_score(y_pred, y_test))}")
+y = df["Cquality"]
+X = df.drop(["Cquality"], axis=1)
+
+MinMaxScaler = preprocessing.MinMaxScaler()
+X = MinMaxScaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+knn_clf = KNeighborsClassifier()
+knn_clf.fit(X_train, y_train)
+ypred = knn_clf.predict(X_test)
+
+result = confusion_matrix(y_test, ypred)
+print("Confusion Matrix:")
+print(result)
+result1 = classification_report(y_test, ypred)
+print("Classification Report:",)
+print(result1)
+result2 = accuracy_score(y_test,ypred)
+print("Accuracy:",result2)
